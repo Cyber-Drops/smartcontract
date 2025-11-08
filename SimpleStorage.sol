e118836f334dc3c393007dd201a64ec2f19a1548
Cyber-Drops <simtemp8@gmail.com>
2025-11-08 18:33:08 +0100

    lezione sulle funzioni

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,5 +3,8 @@ pragma solidity 0.8.19;
 
 
 contract SimpleStorage{
-    uint256 numeroPreferito; //predefinito e' 0              
+    uint256 public numeroPreferito; //predefinito e' 0       
+    function store(uint256 _numeroPreferito) public{
+        numeroPreferito = _numeroPreferito;
+    }       
 }
